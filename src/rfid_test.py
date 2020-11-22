91c2b24f94d4461b2042c1b217aa77f3d3a1a6bf
Marco Campos <marco.campos001@gmail.com>
2020-11-22 00:15:35 +0000

    Reading rfid tag

--- src/rfid_test.py
+++ src/rfid_test.py
@@ -1,8 +1,13 @@
-import serial
+from serial import Serial
+import time
 
-port = serial.Serial('/dev/ttyS0', 9600, timeout=0.2)
+brate = 9600
+port = Serial('/dev/ttyAMA0', 9600, timeout=0.2)
 
 while True:
+    print("Waiting")
     rcv = port.readline()
-    if len(rcv) > 10:
+    rcv = ":".join("{:02x}".format(ord(c)) for c in rcv)
+    if len(rcv) > 1:
         print("Tag: {}".format(str(rcv)))
+    time.sleep(1)
--- src/test2.py
+++ src/test2.py
@@ -1,5 +1,9 @@
 import serial
-
-ser = serial.Serial("myUsbPortID", 9600)
-by = ser.inWaiting()
-print(ser.read(by))
+import time
+while True:
+    print("wait")
+    ser = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.2)
+    by = ser.inWaiting()
+    data = ":".join("{02x}".format(ord(by)))
+    print(ser.read(data))
+    time.sleep(5)
